2bf97145ff1df74739a96cb2600d9f7f6cf5c52f
Hugh Rawlinson <hughr2005@gmail.com>
2021-08-02 10:53:32 +0200

    add ngrok snake example

---
+++ packages/examples/ngrok_snake.ts
@@ -0,0 +1,43 @@
+import { BattleSnake } from "battlesnake";
+import { battlesnakeNgrok } from "battlesnake-plugin-ngrok";
+
+const mySnake = BattleSnake(
+  "super-perfect-snake",
+  {
+    apiversion: "1",
+    author: "",
+    color: "#888888",
+    head: "default",
+    tail: "default",
+    version: "0.0.1",
+  },
+  {
+    plugins: [battlesnakeNgrok()],
+  }
+);
+
+mySnake.onStartGame((gameData) => {
+  console.log(gameData);
+
+  console.log("START");
+});
+
+mySnake.onMove((gameData) => {
+  console.log(gameData);
+
+  var possibleMoves = ["up", "down", "left", "right"] as const;
+  var move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
+
+  console.log("MOVE: " + move);
+  return {
+    move,
+  };
+});
+
+mySnake.onEndGame((gameData) => {
+  console.log(gameData);
+
+  console.log("END");
+});
+
+mySnake.start();
